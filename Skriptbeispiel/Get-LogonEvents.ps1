02f4ecd20c78966f97101c9cbc7346d79c7a09b6
Stefan Ober <STefanO@ppedv.de>
2020-12-11 10:52:20 +0100

    Einfache Parameter eingeführt

--- Skriptbeispiel/Get-LogonEvents.ps1
+++ Skriptbeispiel/Get-LogonEvents.ps1
@@ -1,2 +1,11 @@
 
-Get-EventLog -LogName Security | Where-Object EventId -eq 4624 | Select-Object -First 10
+param(
+$Logname,
+$Computername,
+$EventId,
+$Newest
+
+)
+
+#Befehlsverkettung mit Variablen
+Get-EventLog -LogName $Logname -ComputerName $Computername | Where-Object EventId -eq $EventId | Select-Object -First $Newest
