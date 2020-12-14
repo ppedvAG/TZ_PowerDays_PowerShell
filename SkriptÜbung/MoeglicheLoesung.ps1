895f4370306ad7bb9318c98f737a5a62b3c75e8f
Stefan Ober <STefanO@ppedv.de>
2020-12-14 10:54:43 +0100

    Moegliche Lösung hinzugefügt mit Function für Dateierstellung

---
+++ SkriptÜbung/MoeglicheLoesung.ps1
@@ -0,0 +1,52 @@
+﻿[cmdletBinding()]
+Param(
+    [ValidateScript({Test-Path -Path $PSItem -IsValid})]
+    [string]$Destinationpath,
+
+    [ValidateRange(1,100)]
+    [int]$FileCount = 9,
+
+    [ValidateRange(1,100)]
+    [int]$DirCount = 4,
+
+    [switch]$Force
+
+)
+
+function New-TestFiles
+{
+    param(
+        $Destinationpath,
+        $FileCount
+    )
+    for($j = 1; $j -le $FileCount; $j++)
+    {
+        $FileName = $Destinationpath + "\Datei" + ("{0:D3}" -f $j) + ".txt"
+        New-Item -Path $FileName -ItemType File
+    }
+
+}
+
+if((Test-Path -Path $Destinationpath))
+{
+    if($Force)
+    {
+        Remove-Item -Path $Destinationpath -Recurse -Force
+    }
+    else
+    {
+        Write-Host -ForegroundColor Red -Object "Ordner bereits vorhanden"
+        exit        
+    }
+}
+
+New-Item -Path $Destinationpath -ItemType Directory
+New-TestFiles -Destinationpath $Destinationpath -FileCount $FileCount
+for($i = 1; $i -le $DirCount;$i++)
+{
+    $DirPath = $Destinationpath +  "\Ordner" + ("{0:D3}" -f $i)
+    New-Item -Path $DirPath -ItemType Directory
+    New-TestFiles -Destinationpath $DirPath -FileCount $FileCount
+}
+
+
