aa8f5cee40638f624f2ea679f3be787094525507
Stefan Ober <STefanO@ppedv.de>
2020-12-15 10:18:13 +0100

    Beispiel für Advanced Parameter, zb mit ParameterSets

---
+++ Skriptbeispiel/AdvancedParameterBeispiel.ps1
@@ -0,0 +1,17 @@
+﻿<#
+.SYNOPSIS 
+    Kurzbeschreibung
+#>
+[cmdletBinding(PositionalBinding=$false)]
+param(
+    [Parameter(Mandatory=$true,ParameterSetName="Set1",ValueFromPipeline=$true)]
+    [Parameter(Mandatory=$false,ParameterSetName="Set2",ValueFromPipeline=$true)] 
+    [string]$param1,
+
+    [Parameter(Mandatory=$true,ParameterSetName="Set2",ValueFromPipelineByPropertyName=$true)]
+    [Parameter(Mandatory=$false,ParameterSetName="Set3",ValueFromPipelineByPropertyName=$true)]    
+    [string]$param2,
+
+    [Parameter(Mandatory=$true,ParameterSetName="Set3",ValueFromPipelineByPropertyName=$true)]
+    [string]$param3
+)
