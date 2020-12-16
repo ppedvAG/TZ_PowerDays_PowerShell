265fda22e345f991503a1220affba576b9e7ddd0
Stefan Ober <STefanO@ppedv.de>
2020-12-16 11:18:53 +0100

    Beispiel für DSC mit ausgelagerter Konfiguration für mehr Dynamic

---
+++ DSC/Dynamic/DSC-Dynamic.ps1
@@ -0,0 +1,20 @@
+﻿configuration WebServer-Dynamic
+{
+    # Zum Abrufen der Knotenliste können Ausdrücke ausgewertet werden.
+    # Beispiel: $AllNodes.Where("Role -eq Web").NodeName
+    node $AllNodes.NodeName
+    {
+       foreach($feature in $Node.WindowsFeatures)
+       {
+            WindowsFeature $feature.Name
+            {
+                Name = $feature.Name
+                Ensure = $feature.Ensure
+            }
+       }      
+    }
+}
+
+#Ausführen des "Konfig" Skriptes wieder notwendig um die Konfig in den Speicher zu laden
+#Hier wird dann auch die psd1 mit angegeben
+WebServer-Dynamic -OutputPath C:\TZ_PowerDays_PowerShell\DSC\Dynamic\Config -ConfigurationData C:\TZ_PowerDays_PowerShell\DSC\Dynamic\WebServerConfig.psd1
---
+++ DSC/Dynamic/WebServerConfig.psd1
@@ -0,0 +1,19 @@
+﻿@{
+    AllNodes = @(
+        @{
+            NodeName = "Member1"
+            WindowsFeatures = @(
+                @{
+                    Name = "Web-Server"
+                    Ensure = "Present"
+                 },
+                 @{
+                    Name = "Web-Mgmt-Tools"
+                    Ensure = "Present"
+                 }
+            )
+          }
+    )
+}
+# Speichert ConfigurationData in einer Datei mit der Dateierweiterung "PSD1".
+#Vorteil mehr Dynamik da die eigentliche Konfiguration ausgegert wird
