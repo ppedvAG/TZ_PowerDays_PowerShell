329ed40bce914cdf8bbdc52e5f0d5cea751bc5b0
Stefan Ober <STefanO@ppedv.de>
2020-12-16 09:57:08 +0100

    Beispiel für Verbose / Debug Ausgabe in Skripten

---
+++ Skriptbeispiel/verbose-debug.ps1
@@ -0,0 +1,16 @@
+﻿[cmdletBinding()]
+param
+(
+    [Parameter(Mandatory=$true)]
+    [string]$Input1
+)
+
+Write-Verbose -Message "Vor erster Ausgabe. Folgender Wert wurde übergeben. Input1: $Input1"
+$Fortschritt += "Schritt1"
+Write-Host -Object $Input1 -ForegroundColor Magenta
+$Fortschritt += "Schritt2"
+Write-Debug -Message "Vor zweiter Ausgabe"
+Write-Host -Object $Input1 -ForegroundColor DarkYellow
+$Fortschritt += "Schritt3"
+Write-Host -Object $Input1 -ForegroundColor Cyan
+$Fortschritt
