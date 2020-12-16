554d5697192bf84402fdae6ce850c3d009d8db96
Stefan Ober <STefanO@ppedv.de>
2020-12-16 10:58:34 +0100

    DSC Example Simple

---
+++ DSC/Simple/DSC-Simple.ps1
@@ -0,0 +1,27 @@
+﻿configuration WebServerConfiguration-Simple
+{
+    # Zum Abrufen der Knotenliste können Ausdrücke ausgewertet werden.
+    # Beispiel: $AllNodes.Where("Role -eq Web").NodeName
+    node ("Member1")
+    {
+        # Ressourcenanbieter aufrufen
+        # Beispiel: WindowsFeature, File
+        WindowsFeature WebServer
+        {
+           Ensure = "Present"
+           Name   = "web-Server"
+        }
+
+        WindowsFeature ManagementTools
+        {
+            Ensure = "Present"
+            Name = "WEb-Mgmt-Tools"
+        }     
+    }
+}
+
+#Diese Konfiguration muss ausgeführt werden um sie in den Speicher zu laden und anschließend die MOF Files zu generieren. 
+#WebServerConfiguration-Simple -OutputPath C:\TZ_PowerDays_PowerShell\DSC\Simple\DSCConfig
+
+#Angewandt kann diese Konfiguration dann mit folgendem cmdlet. Optional mit -Wait . "Blockiert" die Shell bis die Ausführung fertig ist.
+#Start-DscConfiguration -Path C:\TZ_PowerDays_PowerShell\DSC\Simple\DSCConfig -Verbose
